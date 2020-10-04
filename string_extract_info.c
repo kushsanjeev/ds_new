38c9ecc76741e99ad473424ae7fa63545d85d55d
annus7741 <71932393+annus7741@users.noreply.github.com>
2020-10-04 12:10:28 +0530

    Update string_extract_info.c

--- string_extract_info.c
+++ string_extract_info.c
@@ -1,6 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void calculate_string_attributes( int characters, int words, int lines,  FILE * file){
+     while ((ch = fgetc(file)) != EOF)
+    {
+        characters++;
+
+   
+        if (ch == '\n' || ch == '\0')
+            lines++;
+
+ 
+        if (ch == ' ' || ch == '\t' || ch == '\n' || ch == '\0')
+            words++;
+    }
+
+    if (characters > 0)
+    {
+        words++;
+        lines++;
+    }
+
+    printf("\n");
+    printf("Total characters = %d\n", characters);
+    printf("Total words      = %d\n", words);
+    printf("Total lines      = %d\n", lines);
+}
+
 int main()
 {
     FILE * file;
@@ -28,29 +54,7 @@ int main()
 
  
     characters = words = lines = 0;
-    while ((ch = fgetc(file)) != EOF)
-    {
-        characters++;
-
-   
-        if (ch == '\n' || ch == '\0')
-            lines++;
-
- 
-        if (ch == ' ' || ch == '\t' || ch == '\n' || ch == '\0')
-            words++;
-    }
-
-    if (characters > 0)
-    {
-        words++;
-        lines++;
-    }
-
-    printf("\n");
-    printf("Total characters = %d\n", characters);
-    printf("Total words      = %d\n", words);
-    printf("Total lines      = %d\n", lines);
+   calculate_string_attributes(characters, words, lines,file);
 
     fclose(file);
 
